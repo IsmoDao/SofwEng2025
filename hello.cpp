3630046649e75791ddf5ed716685e50836cd3bc8
Ismael Dao <e64732u@etu.univ-lorraine.fr>
2025-09-18 11:25:19 +0200

    cmake version

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,18 @@
 #include<iostream>
+#include <HelloProjConfig.h>
 
-void hello()
-{
-    std::cout<<"Hello World"<<std::endl;
+void hello_world() {
+	std::cout << "Hello World!\n";
 }
-
-int main(){
-    hello();
-    return 0;
+void print_version() {
+	std::cout << "project version is "
+		<< HelloProj_VERSION_MAJOR
+		<< "."
+		<< HelloProj_VERSION_MINOR
+		<< std::endl;
+}
+int main()
+{
+	hello_world();
+	print_version();
 }
