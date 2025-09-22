ef215c89834dbbf95653805f5d897da35f48ca32
Ismael Dao <e64732u@etu.univ-lorraine.fr>
2025-09-22 15:51:10 +0200

    Dockfile

---
+++ .bash_history
@@ -0,0 +1,66 @@
+cmake
+echo hello
+ls
+sl
+ls
+cd root/
+mkdir build
+ls
+
+cmake -G "Ninja" ..
+cmake -G "Ninja" ..
+cmake -G "Ninja" ..
+ls
+cd ..
+mkdir build_docker
+cd build_docker/
+cmake -G "Ninja" ..
+
+docker build -t hello_world_image .
+exit
+ls
+cd MyProject/
+ls
+cd.. 
+cd ..
+exit
+ls
+cd MyProject/
+ls
+cd build_my/
+ls
+exit
+cd MyProject/build_my/
+ls
+exit
+ls
+exit 
+exit
+ls
+sl
+exit
+ls
+cd root/
+ls
+cd mains/
+ls
+cd ..
+cd build
+cd ..
+ls
+cd ..
+cd ..
+ls
+cd bin
+ls
+ls ..
+cd ..
+cd ..
+ls
+HelloWord
+HelloWord
+./HelloWorld
+exit
+ls
+HelloWorld
+exit
--- mains/hello.cpp
+++ mains/hello.cpp
@@ -1,23 +1,23 @@
-#include<iostream>
 #include <HelloProjConfig.h>
-#include<basic/message.h>
-
+#include <basic/message.h>
+#include <iostream>
 
 /*
 void hello_world() {
-	std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
 }
 void print_version() {
-	std::cout << "project version is "
-		<< HelloProj_VERSION_MAJOR
-		<< "."
-		<< HelloProj_VERSION_MINOR
-		<< std::endl;
+    std::cout << "project version is "
+        << HelloProj_VERSION_MAJOR
+        << "."
+        << HelloProj_VERSION_MINOR
+        << std::endl;
 }
 */
 
 int main()
 {
-	hello_world();
-	print_version();
+    hello_world();
+    print_version();
+    return 0;
 }
